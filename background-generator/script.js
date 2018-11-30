256d364b23918b8f1e2e62b9827007af165b32f2
Gavin Robertson <gavinrob92@gmail.com>
2018-11-29 20:50:41 -0700

    added background generator code

---
+++ background-generator/script.js
@@ -0,0 +1,30 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.querySelector("button");
+
+window.onload = setGradient();
+
+function setGradient() {
+	body.style.background =
+		"linear-gradient(to right, " +
+		color1.value +
+		", " +
+		color2.value +
+		")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function getRandomColors() {
+	color1.value = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+	color2.value = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+	setGradient();
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", getRandomColors)
